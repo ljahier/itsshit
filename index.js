40f3e7462c183ce491c65df2606edf84f32b6c96
Lucas JAHIER <lucas.jahier1@gmail.com>
2017-07-13 23:07:32 +0200

    add mysql

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
+/*
+ * developed with love by Lucas JAHIER https://github.com/ljahier
+ */
+
 var express = require('express')
 var subDomain = require('express-subdomain')
 var exphbs = require('express-handlebars')
 var mysql = require('mysql')
-var config = require('./config')
-// var http = require('http')
+var config = require('./config.json')
 var app = express()
 var port = 3000
 
-console.log(config.database.password)
-
+var con = mysql.createConnection({
+    host: config.database.host,
+    database: config.database.database,
+    user: config.database.user,
+    port: config.database.port,
+    password: config.database.password
+})
+con.connect(function (err) {
+    if (err) throw err
+    console.log("Database work!")
+})
 app.engine('handlebars', exphbs({
     defaultLayout: 'main'
 }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public/'))
-
 app.get('/', (req, res) => {
     let mydomain = req.hostname
     let subdomain = mydomain.split('.')
@@ -23,23 +34,34 @@ app.get('/', (req, res) => {
         res.render('page', {
             subdomain: subdomain[0]
         })
+
+        /*var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("1 record inserted");
+        });*/
+
         return 0
     }
     res.render('home')
 })
-
 app.get('/login', (req, res) => {
     res.render('login')
 })
-
 app.get('/register', (req, res) => {
     res.render('register')
 })
-
-app.get('*', function(req, res){
+app.get('*', function (req, res) {
     res.redirect('/')
 })
 
+function lol() {
+    Date.now = function now() {
+        console.log(new Date().getTime())
+    }
+}
+
+lol()
 
 app.listen(port, () => {
     console.log('localhost:%s', port)
