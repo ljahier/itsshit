aef29df51738d6c9c00b412b8dc3d78817a50849
Lucas <lucas.jahier1@gmail.com>
2017-10-10 18:52:01 +0200

    update routes.js file

--- app/routes.js
+++ app/routes.js
@@ -15,7 +15,7 @@ module.exports = function (app, mysql, config, subDomain, exphbs, sha256, bodyPa
         let mydomain = req.hostname
         let subdomain = mydomain.split('.')
 
-        if (subdomain.length >= 2) { // For prod change the length 2 are 3
+        if (subdomain.length >= 3) { // For prod change the length 2 are 3
             res.redirect('http://' + subdomain[1] + ':' + port)
         } else {
             res.redirect('/')
